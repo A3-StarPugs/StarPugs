99c290d1daf25231b17b7faaf205ca1563ed9e84
dsfdanilo <danilo.s.franca@outlook.com>
2024-06-06 21:10:46 -0300

    Tela Shop adicionada

---
+++ src/app/shop/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar/Navbar";
+import Header from "@/components/Shop/Header";
+import Products from "@/components/Shop/Products";
+import Footer from"@/components/Footer/Footer";
+
+export default function Shop()  {
+  return (
+    <main>
+      <Navbar/>
+      <Header/>
+      <Products/>
+      <Footer/>
+    </main>
+  );
+}
