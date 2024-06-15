5f037ded727372de893abea86d185e80015a630f
Caio Santos <78771496+CaioSan7os@users.noreply.github.com>
2024-06-15 11:57:33 -0300

    Add files via upload
    uma pequena alteração no botão "CONHEÇA", linkando ele a page "Sobre Nós"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,24 @@
-import styles from './Header.module.scss'
-
+import styles from "./Header.module.scss";
+import Link from "next/link";
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.content}>
         <h1>STARPUGS</h1>
         <div className={styles.line} />
-        <p className='dark-bg'>
-          Se você busca um lugar acolhedor e pet-friendly, onde possa desfrutar de um delicioso café na companhia do seu
-          amigo de quatro patas, venha conhecer o Starpugs. Nosso espaço foi cuidadosamente projetado para proporcionar
-          uma experiência única, onde você e seu pet serão recebidos com todo o carinho e atenção que merecem.
+        <p className="dark-bg">
+          Se você busca um lugar acolhedor e pet-friendly, onde possa desfrutar
+          de um delicioso café na companhia do seu amigo de quatro patas, venha
+          conhecer o Starpugs. Nosso espaço foi cuidadosamente projetado para
+          proporcionar uma experiência única, onde você e seu pet serão
+          recebidos com todo o carinho e atenção que merecem.
         </p>
-        <button className={styles.btn}>Conheça</button>
+       
+         <button className={styles.btn}>
+         <Link href="/sobreNos">Conheca</Link> 
+          </button>
+          
       </div>
     </header>
-  )
+  );
 }
