33efd0028bd114b968c98722586466962174f928
Caio Santos <78771496+CaioSan7os@users.noreply.github.com>
2024-06-15 11:52:05 -0300

    Add files via upload

---
+++ src/app/sobreNos/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
+import SobreNos from "@/components/SobreNos/SobreNos";
+import NavbarSobreNos from "@/components/NavbarSobreNos/Navbar";
+
+export default function SobreNosPage() {
+    return (
+      <main>
+      <NavbarSobreNos />
+      <SobreNos />
+      <Footer />
+    </main>
+    )
+    
+   
+  }
